ae59f3269ae17f4045dd8064d53b4562f76c9725
krushallochawala <krushallochawala@gmail.com>
2025-05-31 18:28:38 +0530

    form designs

---
+++ src/main/java/Client/Url.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Client;
+
+/**
+ *
+ * @author DELL
+ */
+public class Url {
+    //Sakshi
+//    public static String ClientURL = "http://localhost:55335/IntershipPortal-adminDashboard/resources";
+    
+    //Krushal
+    public static String ClientURL = "http://localhost:8080/Online-Internship-Portal-AdminDashboard/resources";
+}
