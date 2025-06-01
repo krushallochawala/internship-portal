4c5bdbc0d8b3bb754627940feb56134e76b34b50
krushallochawala <krushallochawala@gmail.com>
2025-06-01 16:06:12 +0530

    forogot password

--- src/main/java/Client/Url.java
+++ src/main/java/Client/Url.java
@@ -10,8 +10,8 @@ package Client;
  */
 public class Url {
     //Sakshi
-    public static String ClientURL = "http://localhost:55335/IntershipPortal-adminDashboard/resources";
+//    public static String ClientURL = "http://localhost:55335/IntershipPortal-adminDashboard/resources";
     
     //Krushal
-    //public static String ClientURL = "http://localhost:8080/Online-Internship-Portal-AdminDashboard/resources";
+    public static String ClientURL = "http://localhost:8080/Online-Internship-Portal-AdminDashboard/resources";
 }
